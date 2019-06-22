737267f63e4cf888617334b739faa00514e76b75
Sarah Ahmad <CoffeeJavaBeans@sarahmahmad.com>
2019-06-22 18:26:05 -0500

    student model tasts pass

--- db/models/student.js
+++ db/models/student.js
@@ -5,8 +5,21 @@ const db = require('../db');
 
 const Student = db.define('Student', {
   firstName: { type: Sequelize.STRING, allowNull: false },
-  lastName: { type: Sequelize.STRING },
-  email: { type: Sequelize.STRING }
+  lastName: { type: Sequelize.STRING, allowNull: false },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true
+    }
+  }
+});
+
+Student.addHook('beforeSave', (student, options) => {
+  student.firstName =
+    student.firstName.charAt(0).toUpperCase() + student.firstName.slice(1);
+  student.lastName =
+    student.lastName.charAt(0).toUpperCase() + student.lastName.slice(1);
 });
 
 module.exports = Student;
